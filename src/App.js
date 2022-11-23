be4136ef11e2d51f7ed6cf082c34197156c382b7
Araf96 <rafatspn@gmail.com>
2022-11-23 11:57:48 +0600

    loader added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
+import Home from "./pages/Home";
+
+import "./App.css";
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello process navigator!!!!!</p>
-    </div>
+    <>
+      <Home />
+    </>
   );
 }
 
