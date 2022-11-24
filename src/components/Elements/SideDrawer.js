165eec187a3883200087d805d45fc11347bd1f71
Araf96 <rafatspn@gmail.com>
2022-11-24 16:48:11 +0600

    side drawer logic added

--- src/components/Elements/SideDrawer.js
+++ src/components/Elements/SideDrawer.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import ReactDOM  from 'react-dom'
+import { CSSTransition } from 'react-transition-group'
 
-export default function SideDrawer() {
-  return (
-    <div>
-      Hello
-    </div>
-  )
+import './SideDrawer.module.css'
+
+export default function SideDrawer(props) {
+  const drawer = (
+    <CSSTransition
+    in={props.show}
+    timeout={500}
+    classNames="slide-in-left"
+    mountOnEnter
+    unmountOnExit
+    >
+      <aside className='side-drawer' onClick={props.onClick}>
+        {props.children}
+      </aside>
+    </CSSTransition>
+  );
+
+  return ReactDOM.createPortal(drawer, document.getElementById('drawer-hook'));
 }
--- src/components/Navigation/Navigator.js
+++ src/components/Navigation/Navigator.js
@@ -1,25 +1,43 @@
 import { NavLink } from "react-router-dom";
 
 import NavLinks from "./NavLinks";
-import SideDrawer from "../Elements/SideDrawer";
+// import SideDrawer from "../Elements/SideDrawer";
 import useWindowDimensions from "../../hooks/window-dimension";
 
 import styles from "./Navigator.module.css";
+import { useState } from "react";
 
 export default function Navigator() {
   const windowWidth = useWindowDimensions();
-  console.log(windowWidth);
+  const [sideDrawerIsOpen, setSideDrawerIsOpen] = useState(false);
+
+  const sideDrawerOpenHandler = () => {
+    setSideDrawerIsOpen(true);
+    console.log(sideDrawerIsOpen);
+  };
+  const sideDrawerCloseHandler = () => {
+    console.log("Side drawer is closed");
+  };
+
   return (
     <>
       <header className={styles.header}>
+      <button className={styles.navMenuBtn} onClick={sideDrawerOpenHandler}>
+          <span />
+          <span />
+          <span />
+        </button>
         <div className={styles.logo}>
           <NavLink to="/">SmartBiz</NavLink>
         </div>
-        <nav>
-          <NavLinks />
-        </nav>
+        {windowWidth > 670 && (
+          <nav>
+            <NavLinks />
+          </nav>
+        )}
+
       </header>
-      <SideDrawer/>
+      {/* {windowWidth <=670 && <SideDrawer show={sideDrawerIsOpen} />} */}
     </>
   );
 }
