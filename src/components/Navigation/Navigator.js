25f16639fa346cc668140944788c3ef3c8873d54
Araf96 <rafatspn@gmail.com>
2022-11-24 14:01:56 +0600

    window dimension hook added

--- src/components/Navigation/Navigator.js
+++ src/components/Navigation/Navigator.js
@@ -1,21 +1,25 @@
 import { NavLink } from "react-router-dom";
 
 import NavLinks from "./NavLinks";
+import SideDrawer from "../Elements/SideDrawer";
+import useWindowDimensions from "../../hooks/window-dimension";
 
 import styles from "./Navigator.module.css";
 
 export default function Navigator() {
+  const windowWidth = useWindowDimensions();
+  console.log(windowWidth);
   return (
     <>
       <header className={styles.header}>
         <div className={styles.logo}>
           <NavLink to="/">SmartBiz</NavLink>
         </div>
-
         <nav>
           <NavLinks />
         </nav>
       </header>
+      <SideDrawer/>
     </>
   );
 }
---
+++ src/hooks/window-dimension.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react"
+
+function getWindowDimensions(){
+    const {innerWidth : width}  = window;
+    return width;
+}
+
+export default function useWindowDimensions () {
+    const [windowWidth, setWindowWidth] = useState(getWindowDimensions());
+
+    useEffect(()=>{
+        function handleResize(){
+            setWindowWidth(getWindowDimensions());
+        }
+
+        window.addEventListener('resize', handleResize);
+        return ()=> window.removeEventListener('resize', handleResize);
+    },[]);
+
+    return windowWidth;
+};
