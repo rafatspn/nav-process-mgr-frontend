11e827d73ebc644f826fbe7171488be786475406
tanzin <tangin31@gmail.com>
2023-01-26 13:46:03 +0600

    leads tab added

---
+++ src/components/Dashboard/Leads.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Leads = () => {
+    return <div>Coming soon..</div>
+}
+
+export default Leads
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,38 +1,42 @@
-import React from "react";
-import { Form, Button, Card, Container, Row, Col } from "react-bootstrap";
-import Tab from "react-bootstrap/Tab";
-import Tabs from "react-bootstrap/Tabs";
+import React from 'react'
+import { Form, Button, Card, Container, Row, Col } from 'react-bootstrap'
+import Tab from 'react-bootstrap/Tab'
+import Tabs from 'react-bootstrap/Tabs'
 
-import LatestNews from "../components/Dashboard/LatestNews";
-import PostPerformance from "../components/Dashboard/PostPerformance";
-import CompetitorAnalysis from "../components/Dashboard/CompetitorAnalysis";
+import LatestNews from '../components/Dashboard/LatestNews'
+import PostPerformance from '../components/Dashboard/PostPerformance'
+import CompetitorAnalysis from '../components/Dashboard/CompetitorAnalysis'
+import Leads from '../components/Dashboard/Leads'
 
 export default function Dashboard() {
-  return (
-    <Container fluid>
-      <div className="row">
-        <div className="col-md-12">
-          <Tabs
-            defaultActiveKey="postPerformance"
-            id="uncontrolled-tab-example"
-            className="mb-3 mt-3"
-          >
-            <Tab eventKey="latestNews" title="Latest News">
-              <LatestNews />
-            </Tab>
-            <Tab
-              className=" ps-5 pe-5 "
-              eventKey="postPerformance"
-              title="Post Performance"
-            >
-              <PostPerformance />
-            </Tab>
-            <Tab eventKey="competitorAnalysis" title="Competitor Analysis">
-              <CompetitorAnalysis />
-            </Tab>
-          </Tabs>
-        </div>
-      </div>
-    </Container>
-  );
+    return (
+        <Container fluid>
+            <div className="row">
+                <div className="col-md-12">
+                    <Tabs
+                        defaultActiveKey="postPerformance"
+                        id="uncontrolled-tab-example"
+                        className="mb-3 mt-3">
+                        <Tab eventKey="latestNews" title="Latest News">
+                            <LatestNews />
+                        </Tab>
+                        <Tab
+                            className=" ps-5 pe-5 "
+                            eventKey="postPerformance"
+                            title="Post Performance">
+                            <PostPerformance />
+                        </Tab>
+                        <Tab
+                            eventKey="competitorAnalysis"
+                            title="Competitor Analysis">
+                            <CompetitorAnalysis />
+                        </Tab>
+                        <Tab eventKey="leads" title="Leads">
+                            <Leads />
+                        </Tab>
+                    </Tabs>
+                </div>
+            </div>
+        </Container>
+    )
 }
