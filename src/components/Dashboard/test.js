cf3e5c470407540250b2ebd456e65e334495708c
atik <atikurrahman8410@gmail.com>
2023-02-11 13:07:44 +0600

    for pull latest code

---
+++ src/components/Dashboard/test.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import './LatestNews.css'
+
+const LatestNews = () => {
+    return (
+        <>
+            {/* <iframe
+                src="http://103.187.95.197:5500/home.html"
+                style={{ height: '100vh', width: '100%' }}></iframe> */}
+            <div className="container-fluid p-4">
+                <div className="row"></div>
+            </div>
+        </>
+    )
+}
+
+export default LatestNews
